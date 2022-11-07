d6dc92f7da638fa7c6cc08f2cf844bc13953a3df
uRo3YA <ltg8693@gmail.com>
2022-11-07 16:39:20 +0900

    Commit Message

--- Restaurant/admin.py
+++ Restaurant/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Restaurant
 
 # Register your models here.
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "addr",
+        "longtitude",
+        "latitude",
+        "tel",
+        "category",
+        "image",
+        "thumbnail",
+        "imgurl",
+        "description",
+    )
+    search_fields = (
+        "title",
+        "addr",
+        "longtitude",
+        "latitude",
+        "tel",
+        "category",
+        "image",
+        "thumbnail",
+        "imgurl",
+        "description",
+    )
+
+
+admin.site.register(Restaurant, RestaurantAdmin)
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from .models import Review, Comment
 
 # Register your models here.
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "content",
+        "created_at",
+        "updated_at",
+        "image",
+        "Restaurant",
+    )
+    search_fields = (
+        "title",
+        "content",
+        "created_at",
+        "updated_at",
+        "image",
+        "Restaurant",
+    )
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        "review",
+        "user",
+        "content",
+        "created",
+        "deleted",
+        "reply",
+    )
+    search_fields = (
+        "review",
+        "user",
+        "content",
+        "created",
+        "deleted",
+        "reply",
+    )
+
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
